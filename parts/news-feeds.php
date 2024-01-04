ea9479c28a9f7906e95a65af1756bc580afa10c3
Lisa <lisaqdebona@gmail.com>
2024-01-04 03:06:29 -0500

    Adjust news feeds styling

--- parts/news-feeds.php
+++ parts/news-feeds.php
@@ -20,7 +20,7 @@ if ( $news->have_posts() ) {
         $imgAlt = ($img) ? $img['title'] : '';
         $content = get_the_content();
         $content = ($content) ? strip_tags(strip_shortcodes($content))  : '';
-        $excerpt = ($content) ? shortenText( $content, 100, ".", "...") : "";
+        $excerpt = ($content) ? shortenText( $content, 600, ".", "...") : "";
         ?>
         <article class="post-item">
           <div class="inside">
@@ -34,10 +34,10 @@ if ( $news->have_posts() ) {
               <?php if($imageUrl) { ?>
                 <a href="<?php echo get_permalink() ?>">
                   <img src="<?php echo $imageUrl?>" alt="<?php echo $imgAlt?>" class="post-image" />
-                  <img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/rectangle-lg.png" alt="" class="resizer" />
+                  <img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/square.png" alt="" class="resizer" />
                 </a>
               <?php } else { ?>
-                <img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/rectangle-lg.png" alt="" />
+                <img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/square.png" alt="" />
               <?php } ?>
             </figure>
           </div>
