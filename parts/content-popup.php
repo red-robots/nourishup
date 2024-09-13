02c11d8cf15ca8c0b2c5f89e5cb58d5037c5cf26
Lisa <lisaqdebona@gmail.com>
2024-09-13 13:54:09 -0400

    Fixed popup message

--- parts/content-popup.php
+++ parts/content-popup.php
@@ -1,5 +1,8 @@
-<?php if( $popup_content = get_field('popup_content','option') ) { ?>
-  <div id="poupContainer" class="popUpWrapper">
+<?php 
+$show_popup = get_field('popup_content_toggle', 'option');
+$is_show = ($show_popup=='on') ? ' style="display:block"':'';
+if( $popup_content = get_field('popup_content','option') ) { ?>
+  <div id="poupContainer" class="popUpWrapper"<?php echo $is_show; ?>>
     <div class="popupContent animated fadeIn">
       <a href="javascript:void(0)" id="popupClose"><span class="sr">Close</span></a>
       <div class="popupText">
